3a832cd00b1ea465257d651706289bc1d724a56e
zain100000 <muhammadzainulabideen292@gmail.com>
2025-09-29 18:23:53 +0500

    email helper modify

--- hugo-backend/helpers/email-helper/email.helper.js
+++ hugo-backend/helpers/email-helper/email.helper.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "gmail",
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
