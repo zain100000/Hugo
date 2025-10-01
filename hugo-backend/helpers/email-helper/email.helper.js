d9f0b47acda307186083dc9cfdbfbbc8f4f6444c
zain100000 <muhammadzainulabideen292@gmail.com>
2025-10-01 14:46:32 +0500

    commit

--- hugo-backend/helpers/email-helper/email.helper.js
+++ hugo-backend/helpers/email-helper/email.helper.js
@@ -2,12 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465, // Standard port for SMTPS (Secure SMTP)
-  secure: true, // Set to true for SMTPS
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 /**
